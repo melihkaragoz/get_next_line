dcaf808c4ba231a0187e69d8b8126272122210ab
melihkaragoz <melihkkaragoz@hotmail.com>
2022-11-11 00:03:10 +0300

    removed unnecessary Makefile

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: mkaragoz <mkaragoz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 02:14:42 by mkaragoz          #+#    #+#             */
-/*   Updated: 2022/11/10 23:53:47 by mkaragoz         ###   ########.fr       */
+/*   Updated: 2022/11/10 23:58:07 by mkaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 #ifndef BUFFER_SIZE // .h dosyasina eklenecek.
-#define BUFFER_SIZE 42
+#define BUFFER_SIZE 3
 #endif
 #include <unistd.h>
 #include <stdlib.h>
